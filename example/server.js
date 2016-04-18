069c64d531dd06566c5bea92b509ae048ad1a713
Alexandre Neves <alexandre-neves@telecom.pt>
2016-04-18 11:14:56 +0100

    build

--- example/server.js
+++ example/server.js
@@ -5,26 +5,27 @@ var app = express();
 app.use(require('morgan')('short'));
 
 (function() {
-  var webpack = require('webpack');
-  var webpackConfig = require('./webpack.config');
-  var compiler = webpack(webpackConfig);
+    var webpack = require('webpack');
+    var webpackConfig = require('./webpack.config');
+    var compiler = webpack(webpackConfig);
 
-  app.use(require("webpack-dev-middleware")(compiler, {
-    noInfo: true, publicPath: webpackConfig.output.publicPath
-  }));
+    app.use(require("webpack-dev-middleware")(compiler, {
+        noInfo: true, publicPath: webpackConfig.output.publicPath
+    }));
 
-  app.use(require("webpack-hot-middleware")(compiler, {
-    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-  }));
+    app.use(require("webpack-hot-middleware")(compiler, {
+        log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+    }));
 })();
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 if (require.main === module) {
-  var server = http.createServer(app);
-  server.listen(process.env.PORT || 1616, function() {
-    console.log("Listening on %j", server.address());
-  });
+    var server = http.createServer(app);
+
+    server.listen(process.env.PORT || 1616, function() {
+        console.log("Listening on %j", server.address());
+    });
 }
