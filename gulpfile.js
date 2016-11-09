de05083ed9e7d1c0ffd84a9d8e73150613c369ca
Alexandre Neves <aneves@grond.home>
2016-11-09 23:06:06 +0000

    compressing

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
     gulp.src('./src/**/*.scss')
-        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./dist'));
 });
 
